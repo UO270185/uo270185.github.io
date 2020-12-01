b118d5313c2d5f5d925c057591e5b3e9a79b9850
UO270185 <60234134+UO270185@users.noreply.github.com>
2020-12-01 15:53:52 +0100

    Ejercicio 1

---
+++ Ejercicio-01/Cabecera.js
@@ -0,0 +1,13 @@
+class Asignatura {
+    constructor() {
+        this.nombre = "Software y estandares en la Web";
+        this.titulacion = "Grado en Ingenierí­a Informática del Software";
+        this.centro = "Escuela de Ingenierí­a Informática";
+        this.universidad = "Universidad de Oviedo";
+        this.curso = "2020-21";
+        this.estudiante = "Sara Rubín Estrada-Nora";
+        this.email = "uo270185@uniovi.es";
+    }
+}
+
+var asignatura = new Asignatura();
